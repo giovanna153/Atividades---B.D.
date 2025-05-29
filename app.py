32dbc12b175f48d078f464ea011f4fcc726c0423
giovanna153 <o.giovanna@escolar.ifrn.edu.br>
2025-05-29 08:34:40 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from flask import Flask
+from datetime import datetime
+import os
 
 app = Flask(__name__)
+arquivo = "acessos.txt"
 
 @app.route("/")
-def olar():
-    return "ciao mundo bello"
+def index():
+    now = datetime.now()
+    data_hora_atual = now.strftime("%d/%m/%Y %H:%M")
 
+    if os.path.exists(arquivo):
+        with open(arquivo, "r") as f:
+            linhas = f.readlines()
+    else:
+        linhas = []
+    linhas = [linha.strip() for linha in linhas if linha.strip()]
 
-@app.route("/nome")
-def nome():
-    return " gio"
+    if len(linhas) == 0:
+        resposta = "Primeiro Acesso"
+    else:
+        resposta = linhas[-1]
 
-@app.route("/magica/<naosei>")
-def magica(naosei):
-    return naosei
+    with open(arquivo, "a") as f:
+        f.write(data_hora_atual + "\n")
+    return f"{resposta}\nData e Hora de acesso: {data_hora_atual}"
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
-@app.route("/<int:num1>/<int:num2>")
-def somar(num1,num2):
-    resultado = num1 + num2
-    return f"resposta: {resultado}"
